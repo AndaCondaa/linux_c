1ec51afa57e769e82af472e023cdcab9ac8f40f8
AndaCondaa <simon5229@naver.com>
2025-02-01 16:22:52 +0000

    init

---
+++ c_study/bit.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main(){
+    char a = -1;
+
+    //char Val = 0;
+    char printVal = 0;
+
+    printVal = a & (1<<7);
+    printVal = printVal >> 7;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<6);
+    printVal = printVal >> 6;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<5);
+    printVal = printVal >> 5;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<4);
+    printVal = printVal >> 4;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<3);
+    printVal = printVal >> 3;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<2);
+    printVal = printVal >> 2;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<1);
+    printVal = printVal >> 1;
+    printf("%d ", printVal);
+
+    printVal = a & (1<<0);
+    printVal = printVal >> 0;
+    printf("%d ", printVal);
+
+    return 0;
+}
---
+++ c_study/first.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void main(){
+    int b = 134;
+    int x = 342;
+    int m = 1234;
+    int a = 3;
+    a = 3;
+    
+    float test1 = 2.3;
+    double test2 = 323.2;
+
+    printf("%d\n", sizeof(test1));
+    printf("%d\n", sizeof(test2));
+
+
+
+    printf("size: %d\n", sizeof(__LINE__));
+    printf("size2: %d\n", sizeof(x));
+    printf("%d\n",a);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+    printf("%d\n", __LINE__);
+
+}
